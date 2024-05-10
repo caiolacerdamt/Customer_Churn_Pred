f858854793c3ae92cb42257ff73da92edfe6a778
caiolacerdamt <caiolacerdamt@gmail.com>
2024-05-09 22:58:13 -0300

    EDA starts

---
+++ utils.py
@@ -0,0 +1,12 @@
+def CardTypetoInt(type):
+    if type == 'DIAMOND':
+        return 4
+    elif type == 'PLATINUM':
+        return 3
+    elif type == 'GOLD':
+        return 2
+    elif type == 'SILVER':
+        return 1
+    
+
+
